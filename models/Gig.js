a71187827c84ce3e8ce050019c4990e6e725f15e
Leandro <fahurleandro@gmail.com>
2021-05-16 20:12:24 -0700

    end with basic

--- models/Gig.js
+++ models/Gig.js
@@ -1,22 +1,51 @@
-const Sequelize = require("sequelize");
-const db = require("../config/db");
+const { Sequelize, Model } = require("sequelize");
 
-const Gig = db.define("gig", {
-  title: {
-    type: Sequelize.STRING,
-  },
-  technologies: {
-    type: Sequelize.STRING,
-  },
-  description: {
-    type: Sequelize.STRING,
-  },
-  budget: {
-    type: Sequelize.STRING,
-  },
-  contact_email: {
-    type: Sequelize.STRING,
-  },
-});
+class Gig extends Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        title: {
+          type: Sequelize.STRING,
+        },
+        technologies: {
+          type: Sequelize.STRING,
+        },
+        description: {
+          type: Sequelize.STRING,
+        },
+        budget: {
+          type: Sequelize.STRING,
+        },
+        contact_email: {
+          type: Sequelize.STRING,
+        },
+      },
+      {
+        tableName: "gigs",
+        sequelize,
+      }
+    );
+  }
+}
+// const Sequelize = require("sequelize");
+// const db = require("../config/db");
 
-module.exports = Gig;
+// const Gig = db.define("gig", {
+//   title: {
+//     type: Sequelize.STRING,
+//   },
+//   technologies: {
+//     type: Sequelize.STRING,
+//   },
+//   description: {
+//     type: Sequelize.STRING,
+//   },
+//   budget: {
+//     type: Sequelize.STRING,
+//   },
+//   contact_email: {
+//     type: Sequelize.STRING,
+//   },
+// });
+
+// module.exports = Gig;
